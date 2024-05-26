77d9511dbdbe5f7251ae58374c79a8fcb4383a32
mahaoji <mahaoji@sjtu.edu.cn>
2024-05-26 20:37:07 +0800

    finish page1 page2 page3

--- image_function.py
+++ image_function.py
@@ -3,6 +3,9 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import io
 import cv2
+def RGB2Grey(RGBImage):
+    return cv2.cvtColor(RGBImage,cv2.COLOR_BGR2GRAY)
+
 def threshold_manual(GreyImage,threshold_value=127):
     ans=GreyImage.copy()
     ans[ans>threshold_value]=255
@@ -10,17 +13,18 @@ def threshold_manual(GreyImage,threshold_value=127):
     return ans
 
 def threshold_Otsu(GreyImage):
+    GreyImage = cv2.cvtColor(GreyImage, cv2.COLOR_BGR2GRAY)
     _, otsu_threshold_image = cv2.threshold(GreyImage, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    otsu_threshold_image = Image.fromarray(otsu_threshold_image)
+    #otsu_threshold_image = Image.fromarray(otsu_threshold_image)
     return otsu_threshold_image
 
 def histogram(GreyImage):
     image_array = np.array(GreyImage)
     plt.figure()
     plt.hist(image_array.ravel(), bins=256, range=[0, 256])
-    plt.title('灰度直方图')
-    plt.xlabel('灰度值')
-    plt.ylabel('像素数')
+    plt.title('Grayscale histogram')
+    plt.xlabel('gray level')
+    plt.ylabel('pixel number')
     
     # 将直方图保存为图片
     buf = io.BytesIO()
@@ -60,31 +64,78 @@ def convolutionAndImageFilters(GreyImage,operator='roberts1'):
             [0, 0, 0],
             [1, 2, 1]
         ], dtype=np.float32),
-        "gaussianfilter": np.array([
+        "gaussian": np.array([
             [1, 2, 1],
             [2, 4, 2],
             [1, 2, 1]
-        ]/16, dtype=np.float32)
+        ], dtype=np.float32)/16,
+        "median": np.array([
+            [1, 2, 1],
+            [2, 4, 2],
+            [1, 2, 1]
+        ], dtype=np.float32)/16
     }
     kernel = kernels.get(operator, "roberts1")
     convolved_image = cv2.filter2D(GreyImage, -1, kernel)
     return Image.fromarray(convolved_image)
 
-def dilation_bin(GreyImage,SE=np.array([[1,1,1],[1,1,1],[1,1,1]],dtype=np.int8)):
-    binImage=GreyImage.copy()
-    threshold_value=127
-    binImage[binImage>threshold_value]=1
-    binImage[binImage<=threshold_value]=0
+def dilation_gray(GreyImage,kernelsize=3):
+    GreyImage = cv2.cvtColor(GreyImage, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((kernelsize,kernelsize), np.uint8)
+    return cv2.dilate(GreyImage, kernel, iterations = 1)
+
+def erosion_gray(GreyImage,kernelsize=3):
+    GreyImage = cv2.cvtColor(GreyImage, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((kernelsize,kernelsize), np.uint8)
+    return cv2.erode(GreyImage, kernel, iterations = 1)
+
+def close_gray(GreyImage,kernelsize=3):
+    GreyImage = cv2.cvtColor(GreyImage, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((kernelsize,kernelsize), np.uint8)
+    return cv2.dilate(cv2.erode(GreyImage, kernel, iterations = 1),kernel,iterations=1)
+
+def open_gray(GreyImage,kernelsize=3):
+    GreyImage = cv2.cvtColor(GreyImage, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((kernelsize,kernelsize), np.uint8)
+    return cv2.erode(cv2.dilate(GreyImage, kernel, iterations = 1),kernel,iterations=1)
+
+
+'''
+
+def dilation_bin(binImage,SE=np.array([[1,1,1],[1,1,1],[1,1,1]],dtype=np.int8)):
 
     input_rows, input_cols = binImage.shape
     kernel_rows, kernel_cols = SE.shape
 
-    convolution_result = np.zeros_like(input_matrix)
+    ans=np.zeros([input_rows + kernel_rows - 1,input_cols + kernel_cols - 1])
+    for i in range(kernel_rows):
+        for j in range(kernel_cols):
+            ans[i:i+input_rows,j:j+input_cols]=ans[i:i+input_rows,j:j+input_cols]+binImage
+    ans[ans>1]=1
+    return ans
 
+def erosion_bin(binImage,SE=np.array([[1,1,1],[1,1,1],[1,1,1]],dtype=np.int8)):
+    input_rows, input_cols = binImage.shape
+    kernel_rows, kernel_cols = SE.shape
+    if (input_rows<kernel_rows or input_cols<kernel_cols):
+        return binImage
+    ans=np.zeros([input_rows - kernel_rows + 1,input_cols - kernel_cols + 1])
     for i in range(input_rows - kernel_rows + 1):
         for j in range(input_cols - kernel_cols + 1):
-            input_submatrix = binImage[i:i+kernel_rows, j:j+kernel_cols]
-            convolution_result[i, j] = np.sum(input_submatrix * kernel)
+            fitpatch=binImage[i:i+kernel_rows,j:j+kernel_cols]
+            tmp=fitpatch+SE
+            tmp[tmp>1]=1
+            if fitpatch==tmp:
+                ans[i,j]=1
+    return
 
+def close_bin(binImage,SE=np.array([[1,1,1],[1,1,1],[1,1,1]],dtype=np.int8)):
+    return(dilation_bin(erosion_bin(binImage,SE),SE))
 
+def open_bin(binImage,SE=np.array([[1,1,1],[1,1,1],[1,1,1]],dtype=np.int8)):
+    return(erosion_bin(dilation_bin(binImage,SE),SE))
 
+def Distance_Transform(binImage,SE=np.array([[1,1,1],[1,1,1],[1,1,1]],dtype=np.int8),startpoint=np.array([0,0],dtype=np.int8)):
+    pass
+
+'''
--- interface.py
+++ interface.py
@@ -1,6 +1,126 @@
 import gradio as gr
-iface = gr.Interface(
+import numpy as np
+import cv2
+import image_function
 
 
-)
-iface.launch()
+with gr.Blocks() as page1:
+    gr.Markdown("""# Histogram analysis and threshold operation""")
+    with gr.Row():
+        input_image=gr.Image()
+        output_image=gr.Image()
+    with gr.Row():
+        output_image2=gr.Image()
+        output_image3=gr.Image()
+    with gr.Row():
+        threshold= gr.Slider(minimum=0, maximum=255, default=127, label="Threshold")
+        with gr.Row():
+            btn0=gr.Button('grey image')
+            btn1=gr.Button('histogram')
+            btn2=gr.Button('threshold_manual')
+            btn3=gr.Button('threshold-Ostu')
+    btn0.click(fn=image_function.RGB2Grey,inputs=input_image,outputs=output_image)
+    btn1.click(fn=image_function.histogram,inputs=output_image,outputs=output_image2)
+    btn2.click(fn=image_function.threshold_manual,inputs=[output_image,threshold],outputs=output_image3)
+    btn3.click(fn=image_function.threshold_Otsu,inputs=output_image,outputs=output_image3)
+page1.title="Histogram analysis and threshold operation"
+
+with gr.Blocks() as page2:
+    gr.Markdown("""# Convolution and Image Filters""")
+    with gr.Row():
+        input_image=gr.Image()
+        output_image=gr.Image()
+    with gr.Row():
+        with gr.Column():
+            operator1=gr.Dropdown(["roberts1",'roberts2','prewitt1',"prewitt2","sobel1","sobel2"],value='roberts1',info='fillters for edge detection',label='edge detection')
+            btn1=gr.Button("process")
+        with gr.Column():
+            operator2=gr.Dropdown(["gaussian","median"],value='gaussian',info='fillters for noise reduction',label='noise reduction')
+            btn2=gr.Button("process")
+    btn1.click(fn=image_function.convolutionAndImageFilters,inputs=[input_image,operator1],outputs=output_image)
+    btn2.click(fn=image_function.convolutionAndImageFilters,inputs=[input_image,operator2],outputs=output_image)
+page2.title="Convolution and Image Filters"
+
+with gr.Blocks() as page3:
+    gr.Markdown("""# dilation,erosion,opening and closing operations in grayscale """)
+    with gr.Row():
+        input_image=gr.Image()
+        output_image=gr.Image()
+    with gr.Row():
+        kernelsize=gr.Slider(minimum=1,maximum=20,default=3,label="kernel size",step=1)
+        btn0=gr.Button('set output as input')
+        btn1=gr.Button('dilation')
+        btn2=gr.Button('erosion')
+        btn3=gr.Button('opening')
+        btn4=gr.Button('closing')
+    btn0.click(fn=lambda x:x,inputs=output_image,outputs=input_image)
+    btn1.click(fn=image_function.dilation_gray,inputs=[input_image,kernelsize],outputs=output_image)
+    btn2.click(fn=image_function.erosion_gray,inputs=[input_image,kernelsize],outputs=output_image)
+    btn3.click(fn=image_function.open_gray,inputs=[input_image,kernelsize],outputs=output_image)
+    btn4.click(fn=image_function.close_gray,inputs=[input_image,kernelsize],outputs=output_image)
+page3.title="dilation,erosion,opening and closing operations in grayscale"
+
+with gr.Blocks() as page4:
+    gr.Markdown("""# Morphological edge detection,reconstruction and gradient""")
+page4.title="Morphological edge detection,reconstruction and gradient"
+
+
+with gr.Blocks() as page5:
+    gr.Markdown("""# dilation,erosion,opening and closing operations in binagry scale """)
+    with gr.Row():
+        input_image=gr.Image()
+        output_image=gr.Image()
+    with gr.Row():
+        kernelsize=gr.Slider(minimum=1,maximum=20,default=2,label="kernel size")
+        btn0=gr.Button('set output as input')
+        btn1=gr.Button('dilation')
+        btn2=gr.Button('erosion')
+        btn3=gr.Button('opening')
+        btn4=gr.Button('closing')
+page5.title="dilation,erosion,opening and closing operations in binagry scale "
+
+with gr.Blocks() as page6:
+    gr.Markdown("""# Morphological distance transform,skeleton and skeleton restoration""")
+page6.title="Morphological distance transform,skeleton and skeleton restoration"
+
+
+from fastapi import FastAPI, Response
+
+
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    html_content = """
+    <html>
+    <head>
+        <title>Image Processing Homework</title>
+    </head>
+    <body>
+        <h1>Image Processing Homework<</h1>
+        <p>Author: 马浩吉</p>
+        <p>Click the links below to navigate:</p>
+        <ul>
+            <li><a href="/page1">Page 1: Histogram analysis and threshold operation</a></li>
+            <li><a href="/page2">Page 2: Convolution and Image Filters</a></li>
+            <li><a href="/page3">Page 3: dilation,erosion,opening and closing operations in grayscale</a></li>
+            <li><a href="/page4">Page 4: Morphological edge detection,reconstruction and gradient</a></li>
+            <li><a href="/page5">Page 5: ilation,erosion,opening and closing operations in binagry scale</a></li>
+            <li><a href="/page6">Page 6: Morphological distance transform,skeleton and skeleton restoration</a></li>
+
+        </ul>
+    </body>
+    </html>
+    """
+    return Response(content=html_content, media_type="text/html")
+
+app = gr.mount_gradio_app(app, page1, path='/page1')
+app = gr.mount_gradio_app(app, page2, path='/page2')
+app = gr.mount_gradio_app(app, page3, path='/page3')
+app = gr.mount_gradio_app(app, page4, path='/page4')
+app = gr.mount_gradio_app(app, page5, path='/page5')
+app = gr.mount_gradio_app(app, page6, path='/page6')
+
+import uvicorn
+uvicorn.run(app,port=1234)
